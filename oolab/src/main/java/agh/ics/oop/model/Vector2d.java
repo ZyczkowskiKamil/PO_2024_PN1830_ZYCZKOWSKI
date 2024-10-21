dfaddd5a056774f6c60a050e7534d1032499064d
ZyczkowskiKamil <zyczkowskik04@gmail.com>
2024-10-21 19:35:50 +0200

    Changed Vector2d contructor to public from package private

--- oolab/src/main/java/agh/ics/oop/model/Vector2d.java
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -4,7 +4,7 @@ public class Vector2d {
     private final int x;
     private final int y;
 
-    Vector2d(int x, int y) {
+    public Vector2d(int x, int y) {
         this.x = x;
         this.y = y;
     }
