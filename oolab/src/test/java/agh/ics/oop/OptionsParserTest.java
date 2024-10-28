3c0aee6023ba68b9374feef4d29ed24f3602a228
ZyczkowskiKamil <zyczkowskik04@gmail.com>
2024-10-28 19:11:39 +0100

    Added empty string test to OptionsParserTest

--- oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
+++ oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -3,11 +3,8 @@ package agh.ics.oop;
 import agh.ics.oop.model.MoveDirection;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static agh.ics.oop.OptionsParser.parseStringToMoveDirection;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OptionsParserTest {
 
@@ -15,29 +12,36 @@ public class OptionsParserTest {
     public void parseStringToMoveDirectionTest() {
         String[] input;
         MoveDirection[] expectedOutput;
+        MoveDirection[] output;
+
+        // empty string test
+        input = new String[] {};
+        expectedOutput = new MoveDirection[] {};
+        output = parseStringToMoveDirection(input);
+        assertArrayEquals(expectedOutput, output);
 
         input = new String[] {"l", "r", "f", "b"};
         expectedOutput = new MoveDirection[] {MoveDirection.LEFT, MoveDirection.RIGHT, MoveDirection.FORWARD, MoveDirection.BACKWARD};
-        assertTrue(Arrays.equals(expectedOutput, parseStringToMoveDirection(input)));
+        assertArrayEquals(expectedOutput, parseStringToMoveDirection(input));
 
         input = new String[] {"ll"};
         expectedOutput = new MoveDirection[] {};
-        assertTrue(Arrays.equals(expectedOutput, parseStringToMoveDirection(input)));
+        assertArrayEquals(expectedOutput, parseStringToMoveDirection(input));
 
         input = new String[] {"rr"};
         expectedOutput = new MoveDirection[] {};
-        assertTrue(Arrays.equals(expectedOutput, parseStringToMoveDirection(input)));
+        assertArrayEquals(expectedOutput, parseStringToMoveDirection(input));
 
         input = new String[] {"ff"};
         expectedOutput = new MoveDirection[] {};
-        assertTrue(Arrays.equals(expectedOutput, parseStringToMoveDirection(input)));
+        assertArrayEquals(expectedOutput, parseStringToMoveDirection(input));
 
         input = new String[] {"bb"};
         expectedOutput = new MoveDirection[] {};
-        assertTrue(Arrays.equals(expectedOutput, parseStringToMoveDirection(input)));
+        assertArrayEquals(expectedOutput, parseStringToMoveDirection(input));
 
         input = new String[] {"l", "l", "l", "dsad", "dafas", "lfa", "yva", "@4", "1", "@"};
         expectedOutput = new MoveDirection[] {MoveDirection.LEFT,MoveDirection.LEFT,MoveDirection.LEFT};
-        assertTrue(Arrays.equals(expectedOutput, parseStringToMoveDirection(input)));
+        assertArrayEquals(expectedOutput, parseStringToMoveDirection(input));
     }
 }
